af6852d957bb13237b38f7555c656ee0c9c1dd4a
zjy4fun <zjy4fun@gmail.com>
2023-10-27 00:07:45 +0800

    refactor: remove useless code

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-const a = 'aaa'
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div>jjjjjjjjjjjjjjjjjjjjjjjjjjjj{a}</div>
-
     <App />
   </React.StrictMode>,
 )
